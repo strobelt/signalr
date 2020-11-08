2ea86a49000176ab88a853951eb8c29e338cf9fb
Luiz Strobelt <lhsperez@gmail.com>
2020-11-08 18:28:03 -0300

    Create simple signalR backend

---
+++ src/Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Chat.Server.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+            => await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
---
+++ src/Startup.cs
@@ -0,0 +1,33 @@
+using Chat.Server.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Chat.Server
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+                endpoints.MapHub<ChatHub>("/chathub");
+            });
+        }
+    }
+}
